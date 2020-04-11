f1ee539fb0a7ae795963d8c035985ee9cea2daa4
rossam611 <samuel.reuther@web.de>
2020-04-11 17:48:13 +0200

    mark R-scripts with not utilised functions

--- R/SR_replace_NA_RF-.R
+++ R/SR_replace_NA_RF-.R
@@ -1,6 +1,6 @@
-SR_replace_NA_RF <- function(df, var = NULL) {    # TODO !!!
+# SR_replace_NA_RF <- function(df, var = NULL) {    # TODO !!!
   #   # Impute missing values with RF
   #   p_load(missForest)
   #   # ...
   #   return(df)
-}
+# }
---
+++ R/SR_select_variables_interactively-.R
@@ -0,0 +1,27 @@
+# # source: https://www.r-bloggers.com/building-a-column-selecter/
+# #
+# SR_select_variables_interactively <- function(df, n = 100) {
+#   # load some libraries
+#   # pacman::p_load(shiny, miniUI, dplyr, DT, rstudioapi)
+#   #
+#   # return_dplyr_code <- match.arg(return_dplyr_code)
+#   stopifnot(is.data.frame(df))
+#   df_head <- utils::head(df, n)
+#   #
+#   ui <- miniUI::miniPage(miniUI::gadgetTitleBar("Have your pick"),
+#                          miniUI::miniContentPanel(DT::dataTableOutput("selection_df", height = "100%")))
+#
+#   server <- function(input, output, session){
+#     options(DT.options = list(pageLength = 10))
+#     output$selection_df <- DT::renderDataTable(df_head, server = FALSE,
+#                                                selection = list(target = "column"))
+#     shiny::observeEvent(input$done, shiny::stopApp(input$selection_df_columns_selected))
+#   }
+#   #
+#   cols_selected <- shiny::runGadget(ui, server)
+#   #
+#   # return dplyr codedf %>% select(datetime, workingday) in *.R-file
+#   df_name <- deparse(substitute(df))
+#   colnames_selected <- colnames(df)[cols_selected] %>% paste(collapse = ", ")
+#   rstudioapi::insertText(paste0(df_name, " %>% dyplr::select(", colnames_selected, ")"))
+# }
