d96f8514f6922715bbb5fe61b69ef225e9ffd1ce
rossam611 <samuel.reuther@web.de>
2020-04-11 13:37:14 +0200

    add more methods to SR_modus()

--- R/SR_modus.R
+++ R/SR_modus.R
@@ -3,6 +3,7 @@
 #' calculate most frequent value in a vector
 #'
 #' @param x vector
+#' @param method integer (default = 1): different methods, with being 1 the fastest
 #'
 #' @return character
 #'
@@ -10,15 +11,23 @@
 #' SR_modus(c("a", "a", "b", "c"))
 #'
 #' @export
-SR_modus <- function(x) {
-  # fastest
-  names(sort(table(x), decreasing = TRUE)[1])
+SR_modus <- function(x, method = 1) {
+  if (method == 1) {
+    # fastest
+    modus <- names(sort(table(x), decreasing = TRUE)[1])
+  }
   #
-  # Alternative 1
-  # names(which.max(table(x)))
+  if (method == 2) {
+    # Alternative 1
+    modus <- names(which.max(table(x)))
+  }
   #
-  # Alternative 2
-  # ux <- n_distinct(x)
-  # ux <- unique(x)
-  # ux[which.max(tabulate(match(x, ux)))]
+  if (method == 3) {
+    # Alternative 2
+    ux <- unique(x)
+    modus <- ux[which.max(tabulate(match(x, ux)))]
+  }
+  #
+  # return result
+  return(modus)
 }
--- tests/testthat/test-SR_modus.R
+++ tests/testthat/test-SR_modus.R
@@ -7,3 +7,13 @@ test_that("numbers", {
   expect_equal(SR_modus(c(1, 2, 3, 3)),
                "3")
 })
+
+test_that("characters, method = 2", {
+  expect_equal(SR_modus(c("a", "b", "b", "c"), method = 2),
+               "b")
+})
+
+test_that("characters, method = 3", {
+  expect_equal(SR_modus(c("a", "b", "b", "c"), method = 3),
+               "b")
+})
