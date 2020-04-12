f6f40e3ada197e14563b9b8bbbc31adcac06e2e3
rossam611 <samuel.reuther@web.de>
2020-04-12 19:05:22 +0200

    added SR_is_number()

--- R/SR_is_number.R
+++ R/SR_is_number.R
@@ -1,3 +1,15 @@
+#' Check if object is a number
+#'
+#' check if object is.numeric(), is.integer() or SR_is_date()
+#'
+#' @param x object
+#'
+#' @return Boolean value TRUE or FALSE
+#'
+#' @example
+#' SR_is_number(42.2)
+#'
+#' @export
 SR_is_number <- function(x) {
   ifelse(is.numeric(x) | is.integer(x) | SR_is_date(x), T, F)
 }
