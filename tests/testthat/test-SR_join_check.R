e3c7e1cc2602132beafc249249d0f5f23eacc2ae
samuelreuther <samuel.reuther@web.de>
2025-05-14 11:14:33 +0200

    Update test-SR_join_check.R

--- tests/testthat/test-SR_join_check.R
+++ tests/testthat/test-SR_join_check.R
@@ -1,7 +1,10 @@
 test_that("function works without error", {
-  expect_equal(SR_join_check(data.frame(a = 1:3,
-                                        b = 1:3),
-                             data.frame(a = c(3, 3:5),
-                                        c = 1:4)),
+  expect_equal(SR_join_check(data.frame(a = 1:15,
+                                        b = 1:15,
+                                        d = rep(1, 15)), # skiped
+                             data.frame(a = 5:24,
+                                        c = 1:20,
+                                        d = rep(1, 20))), # skiped
                NULL)
 })
+
